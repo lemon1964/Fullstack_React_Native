fcd9ed1339e37a4d4d6adaee856b31a80713ff4e
lemon <lemon@example.com>
2024-09-20 19:50:40 +0300

    initial

---
+++ rate-repository-app/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Setting up the development environment. 
+        Development environment - mobile application. 
+        Expo Version sdk-51
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
