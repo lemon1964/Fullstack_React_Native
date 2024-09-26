78be103aa9f792c0a0d9d076d193cea441e544d1
lemon <lemon@example.com>
2024-09-26 17:48:39 +0300

    temp3

--- rate-repository-app-c/src/graphql/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const SIGN_IN = gql`
-  mutation SignIn($username: String!, $password: String!) {
-    authenticate(credentials: { username: $username, password: $password }) {
-      accessToken
-    }
-  }
-`;
